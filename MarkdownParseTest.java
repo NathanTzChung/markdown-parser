4cab7281f894a63675c41584e195935ecf4e8c5a
NathanTzChung <ntchung@ucsd.edu>
2022-04-22 17:39:34 -0700

    Tested the new test files (2-8)
    Had to use absolute paths since just putting the file name was not working.

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -17,7 +17,7 @@ public class MarkdownParseTest {
     //testing test-file.md
     @Test
     public void testfile() throws IOException {
-        Path fileName = Path.of("test-file.md");
+        Path fileName = Path.of("C:/UCSD_2021-2022/Q3/CSE 15L/4_15_Lab3/markdown-parser/test-file.md");
         String content = Files.readString(fileName);
         assertEquals(List.of("https://something.com", "some-thing.html"), MarkdownParse.getLinks(content));
     }
@@ -25,22 +25,72 @@ public class MarkdownParseTest {
     //testing the rest of the files
     @Test
     public void break1() throws IOException {
-        Path fileName = Path.of("break.md");
+        Path fileName = Path.of("C:/UCSD_2021-2022/Q3/CSE 15L/4_15_Lab3/markdown-parser/break.md");
         String content = Files.readString(fileName);
         assertEquals(List.of("https://google.com"), MarkdownParse.getLinks(content));
     }
 
     @Test
     public void break2() throws IOException {
-        Path fileName = Path.of("break2.md");
+        Path fileName = Path.of("C:/UCSD_2021-2022/Q3/CSE 15L/4_15_Lab3/markdown-parser/break2.md");
         String content = Files.readString(fileName);
         assertEquals(List.of("https://google.com", "https://twitch.tv"), MarkdownParse.getLinks(content));
     }
 
     @Test
     public void break3() throws IOException {
-        Path fileName = Path.of("break3.md");
+        Path fileName = Path.of("C:/UCSD_2021-2022/Q3/CSE 15L/4_15_Lab3/markdown-parser/break3.md");
         String content = Files.readString(fileName);
         assertEquals(List.of("https://google.com", "https://twitch.tv"), MarkdownParse.getLinks(content));
+    }
+
+    //Part 6 for lab4 Writeup
+    @Test
+    public void testfile2() throws IOException {
+        Path fileName = Path.of("C:/UCSD_2021-2022/Q3/CSE 15L/4_15_Lab3/markdown-parser/test-file2.md");
+        String content = Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testfile3() throws IOException {
+        Path fileName = Path.of("C:/UCSD_2021-2022/Q3/CSE 15L/4_15_Lab3/markdown-parser/test-file3.md");
+        String content = Files.readString(fileName);
+        assertEquals(List.of(), MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testfile4() throws IOException {
+        Path fileName = Path.of("C:/UCSD_2021-2022/Q3/CSE 15L/4_15_Lab3/markdown-parser/test-file4.md");
+        String content = Files.readString(fileName);
+        assertEquals(List.of(), MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testfile5() throws IOException {
+        Path fileName = Path.of("C:/UCSD_2021-2022/Q3/CSE 15L/4_15_Lab3/markdown-parser/test-file5.md");
+        String content = Files.readString(fileName);
+        assertEquals(List.of(), MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testfile6() throws IOException {
+        Path fileName = Path.of("C:/UCSD_2021-2022/Q3/CSE 15L/4_15_Lab3/markdown-parser/test-file6.md");
+        String content = Files.readString(fileName);
+        assertEquals(List.of(), MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testfile7() throws IOException {
+        Path fileName = Path.of("C:/UCSD_2021-2022/Q3/CSE 15L/4_15_Lab3/markdown-parser/test-file7.md");
+        String content = Files.readString(fileName);
+        assertEquals(List.of(), MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testfile8() throws IOException {
+        Path fileName = Path.of("C:/UCSD_2021-2022/Q3/CSE 15L/4_15_Lab3/markdown-parser/test-file8.md");
+        String content = Files.readString(fileName);
+        assertEquals(List.of("a link on the first line"), MarkdownParse.getLinks(content));
     }
 }
