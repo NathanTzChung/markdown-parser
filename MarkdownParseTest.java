6df5e0b129020f91beac84311a65fca7b63f7a0a
NathanTzChung <ntchung@ucsd.edu>
2022-04-22 17:13:13 -0700

    Testing the rest of the files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -22,5 +22,25 @@ public class MarkdownParseTest {
         assertEquals(List.of("https://something.com", "some-thing.html"), MarkdownParse.getLinks(content));
     }
 
-    
+    //testing the rest of the files
+    @Test
+    public void break1() throws IOException {
+        Path fileName = Path.of("break.md");
+        String content = Files.readString(fileName);
+        assertEquals(List.of("https://google.com"), MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void break2() throws IOException {
+        Path fileName = Path.of("break2.md");
+        String content = Files.readString(fileName);
+        assertEquals(List.of("https://google.com", "https://twitch.tv"), MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void break3() throws IOException {
+        Path fileName = Path.of("break3.md");
+        String content = Files.readString(fileName);
+        assertEquals(List.of("https://google.com", "https://twitch.tv"), MarkdownParse.getLinks(content));
+    }
 }
