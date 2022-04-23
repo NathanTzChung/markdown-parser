d329c9c3b25a2fee2dc272047e53bbc2d6bbdf06
NathanTzChung <ntchung@ucsd.edu>
2022-04-22 17:11:25 -0700

    Testing test-file.md

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,26 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
     //Leaving this as a comment so I can commit again with the commands used to run the test
+
+
+    //testing test-file.md
+    @Test
+    public void testfile() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-thing.html"), MarkdownParse.getLinks(content));
+    }
+
+    
 }
